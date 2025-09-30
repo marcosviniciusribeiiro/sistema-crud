e6e4786ccd6318f2f64ec9f426cd5576748500cf
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-09-30 10:35:27 -0300

    Create ProdutoMapper.java

---
+++ src/main/java/com/sistema/crud/mapper/ProdutoMapper.java
@@ -0,0 +1,21 @@
+package com.sistema.crud.mapper;
+
+import com.sistema.crud.dto.ProdutoDTO;
+import com.sistema.crud.model.Produto;
+
+public class ProdutoMapper {
+	public static Produto toEntity(ProdutoDTO dto) {
+		Produto produto = new Produto();
+		produto.setNome(dto.getNome());
+		produto.setDescricao(dto.getDescricao());
+		produto.setPreco(dto.getPreco());
+		return produto;
+	}
+	public static ProdutoDTO toDTO(Produto p) {
+		ProdutoDTO dto = new ProdutoDTO();
+		dto.setNome(p.getNome());
+		dto.setDescricao(p.getDescricao());
+		dto.setPreco(p.getPreco());
+		return dto;
+	}
+}
