50fb414fd5db28641033b9cdbc660e0cdbbdd860
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-02 12:08:40 -0300

    Update ProdutoController.java

--- src/main/java/com/sistema/crud/controller/ProdutoController.java
+++ src/main/java/com/sistema/crud/controller/ProdutoController.java
@@ -2,23 +2,25 @@ package com.sistema.crud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sistema.crud.dto.ProdutoDTO;
-import com.sistema.crud.mapper.ProdutoMapper;
-import com.sistema.crud.repository.ProdutoRepository;
+import com.sistema.crud.service.ProdutoService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
 @Controller
 public class ProdutoController {
-	@Autowired
-	public ProdutoRepository repository;
+	
+	public ProdutoService service;
+	
+	public ProdutoController(ProdutoService service) {
+		this.service = service;
+	}
 	
 	@GetMapping("/cadastro")
 	public String formulario(Model model) {
@@ -28,10 +30,29 @@ public class ProdutoController {
 	
 	@PostMapping("/cadastro")
 	public String salvarProduto(@ModelAttribute("produtoDTO") @Valid ProdutoDTO produtoDTO, BindingResult result) {
-		if(result.hasErrors()) {
+		if(result.hasErrors()){
 			return "index";
 		}
-		repository.save(ProdutoMapper.toEntity(produtoDTO));
+		service.salvarProduto(produtoDTO);
 		return "redirect:/cadastro";
+	}
+	
+	@GetMapping("/produtos")
+	public String listarProdutos(Model model) {
+		model.addAttribute("produtos", service.listarProdutos());
+		return "listagem";
+	}
+
+	@GetMapping("/produtos/editar/{id}")
+	public String editarProduto(@PathVariable Long id, Model model) {
+		ProdutoDTO dto = service.buscarPorId(id);
+		model.addAttribute("produtoDTO", dto);
+		return "index";
+	}
+	
+	@PostMapping("/produtos/deletar/{id}")
+	public String deletarProduto(@PathVariable Long id, Model model) {
+		service.deletarProduto(id);
+		return "redirect:/produtos";
 	}
 }
