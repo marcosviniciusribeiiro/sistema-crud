81c5b93d6915616b70d2206242ef4aef283af55c
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-02 12:07:26 -0300

    Update ProdutoMapper.java

--- src/main/java/com/sistema/crud/mapper/ProdutoMapper.java
+++ src/main/java/com/sistema/crud/mapper/ProdutoMapper.java
@@ -6,6 +6,7 @@ import com.sistema.crud.model.Produto;
 public class ProdutoMapper {
 	public static Produto toEntity(ProdutoDTO dto) {
 		Produto produto = new Produto();
+		produto.setId(dto.getId());
 		produto.setNome(dto.getNome());
 		produto.setDescricao(dto.getDescricao());
 		produto.setPreco(dto.getPreco());
@@ -13,6 +14,7 @@ public class ProdutoMapper {
 	}
 	public static ProdutoDTO toDTO(Produto p) {
 		ProdutoDTO dto = new ProdutoDTO();
+		dto.setId(p.getId());
 		dto.setNome(p.getNome());
 		dto.setDescricao(p.getDescricao());
 		dto.setPreco(p.getPreco());
