3008e025843b3d83cc37c3a3e941f6a0b8e5e0d6
Marcos Vinícius <Marcosvini.rg@gmail.com>
2025-10-07 09:17:34 -0300

    Update ProdutoRepository.java

--- src/main/java/com/sistema/crud/repository/ProdutoRepository.java
+++ src/main/java/com/sistema/crud/repository/ProdutoRepository.java
@@ -2,5 +2,7 @@ package com.sistema.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sistema.crud.model.Produto;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> { }
